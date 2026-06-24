393b8102400c719df01f25f875bd4feba381cafc
Gabriel Evanoff <gevanoff@gmail.com>
2026-06-23 17:06:46 -0700

    Caught a few more ai3 references -> migraine

--- deploy/scripts/_common.sh
+++ deploy/scripts/_common.sh
@@ -183,7 +183,7 @@ ns_gateway_extra_host_env_keys() {
   printf '%s\n' \
     NEXUS_HOST_AI1_IP \
     NEXUS_HOST_AI2_IP \
-    NEXUS_HOST_AI3_IP \
+    NEXUS_HOST_MIGRAINE_IP \
     NEXUS_HOST_ADA2_IP \
     NEXUS_HOST_MELTDOWN_IP \
     NEXUS_HOST_COPYFAIL_IP \
